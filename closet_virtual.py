0a6c83f4a3bb85ef6320fdc3a06031d5572f91b4
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-25 23:57:01 -0300

    importação função sleep e correçao user.

--- closet_virtual.py
+++ closet_virtual.py
@@ -1,11 +1,18 @@
 import mysql.connector
+from time import sleep
 
 conexao = mysql.connector.connect(
     host = 'localhost',
-    user = 'root',
-    password = ' 6joiasthanos',
+    user = 'CesarOli',
+    password = '6joiasthanos',
     database = 'closet_virtual'
 )
 # Verificando conexão
 if conexao.is_connected():
+    sleep(2.5)
+    print('Aguardando conexão ao banco de dados...')
+    sleep(1.1)
     print('Conexão estabelecida com sucesso!!')
+
+
+
