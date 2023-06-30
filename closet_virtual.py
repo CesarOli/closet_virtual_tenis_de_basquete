d3dc68a9207c77072136e6aa74cb5b32878ac759
CesarOli <cesaraleoliveira@gmail.com>
2023-06-30 17:08:56 -0300

    Melhorando e finalizando a apresentação do Projeto.

--- closet_virtual.py
+++ closet_virtual.py
@@ -47,23 +47,52 @@ verificaExisteTabelaTenis(cursor)
 
 def menuPrincipal():
     print('Bem-vindo ao Closet Virtual!')
+    sleep(1)
     while True:
         print('Selecione uma opção:')
+        sleep(0.5)
         print('1. Adicione seu Novo Tênis')
+        sleep(0.5)
         print("0. Sair")
 
+        sleep(0.3)
         opcao = input('Digite o número da opção desejada: ')
 
         if opcao == '1':
-          # adicionarNovoTenis()
+            adicionarNovoTenis()
         elif opcao == '0':
-            print('Obrigado por usar o Closet Virtual! Até mais!')
+            sleep(1)
+            print('Parabéns, seu novo Tênis foi adicionado ao seu Closet Virtual!!')
+            
             break
         else:
             print('Opção inválida. Por favor, selecione uma opção válida.')
 
+def adicionarNovoTenis():
+    print('Adicione seu novo Tênis')
+
+    nome = input('Informe o nome do Tênis: ')
+    marca = input('Informe a marca do Tênis: ')
+    numeracao = input('Informe a numeração do Tênis: ')
+    linha = input('Informe a linha do Tênis: ')
+
+    if conectar is None or not conectar.is_connected():
+        print('Não há uma conexão estabelecida com o banco de dados.')
+        return
+
+    cursor = conectar.cursor()
+
+    inserirNovoTenis = '''INSERT INTO Tenis (nome, marca, numeracao, linha)
+    VALUES (%s, %s, %s, %s)'''
+
+    infoTenis = (nome, marca, numeracao, linha)
+    cursor.execute(inserirNovoTenis, infoTenis)
+    conectar.commit()
+
+menuPrincipal()
+
 '''
-# Verificando conexão
+# Verificando conexão\
 
 
 cursor = conexao.cursor()
@@ -130,6 +159,8 @@ sleep(1.5)
 menuPrincipal()
 conexao.commit()
 conexao.close()'''
+sleep(1.5)
+print('Obrigado por usar o Closet Virtual! Até mais!')
 sleep(2)
 print('\n', 'Fim do Programa!!')
 
